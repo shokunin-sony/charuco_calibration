9ab6c77a25da983e8736adac3f8a1e78a21b714a
Tom Chollet <tomchollet2000@gmail.com>
2023-07-13 15:32:15 +0900

    time now parameters added

--- charuco_calibration/src/calibrate_camera_ros.cpp
+++ charuco_calibration/src/calibrate_camera_ros.cpp
@@ -207,6 +207,7 @@ int main(int argc, char *argv[]) {
     {
         try {
             std::filesystem::create_directory(outputFolder + "deprecated/");
+            auto now = std::chrono::system_clock::now();
             auto in_time_t = std::chrono::system_clock::to_time_t(now);
             std::stringstream datetime;
             datetime << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
